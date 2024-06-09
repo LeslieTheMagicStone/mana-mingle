9dabdf45470f329cd9c920471a174e854f9f3d85
LeslieTheMagicStone <lesliethemagicstone@gmail.com>
2024-06-10 03:31:48 +0800

    feat: network chat

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -3,25 +3,45 @@ using TMPro;
 using UnityEngine.UI;
 using Unity.Netcode;
 
-public class GameLogic : MonoBehaviour
+public class GameLogic : NetworkBehaviour
 {
     [SerializeField] private TMP_InputField input;
     [SerializeField] private RectTransform content;
     [SerializeField] private GameObject origChatCell;
     [SerializeField] private Button sendButton;
 
-    private void Awake() {
+    public override void OnNetworkSpawn()
+    {
         sendButton.onClick.AddListener(OnSendClick);
     }
 
-    private void OnSendClick(){
-        if(string.IsNullOrEmpty(input.text)) return;
+    private void OnSendClick()
+    {
+        if (string.IsNullOrEmpty(input.text)) return;
 
-        PlayerInfo playerInfo  = GameManager.instance.playerInfos[NetworkManager.Singleton.LocalClientId];
+        PlayerInfo playerInfo = GameManager.instance.playerInfos[NetworkManager.LocalClientId];
         AddChatCell(playerInfo.name, input.text);
+
+        if (IsServer) SendMsgToOthersClientRpc(playerInfo, input.text);
+        else SendMsgToOthersServerRpc(playerInfo, input.text);
     }
 
-    private void AddChatCell(string playerName, string content){
+    [ClientRpc]
+    void SendMsgToOthersClientRpc(PlayerInfo playerInfo, string content)
+    {
+        if (IsHost || playerInfo.id == NetworkManager.LocalClientId) return;
+        AddChatCell(playerInfo.name, content);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    void SendMsgToOthersServerRpc(PlayerInfo playerInfo, string content)
+    {
+        AddChatCell(playerInfo.name, content);
+        SendMsgToOthersClientRpc(playerInfo, content);
+    }
+
+    private void AddChatCell(string playerName, string content)
+    {
         GameObject clone = Instantiate(origChatCell);
         clone.transform.SetParent(this.content, false);
         clone.AddComponent<ChatCell>().Init(playerName, content);
