3bb9d0a52fe572377656a5d8ade982a6e8f419aa
LeslieTheMagicStone <lesliethemagicstone@gmail.com>
2024-06-17 19:01:31 +0800

    feat: final

--- Assets/Scripts/SpellPreview.cs
+++ Assets/Scripts/SpellPreview.cs
@@ -25,33 +25,33 @@ public class SpellPreview : MonoBehaviour
 
     public void SetTransparency(float alpha)
     {
-        Renderer renderer = transform.GetChild(0).GetComponent<Renderer>();
-        if (renderer != null)
-        {
-            Color color = renderer.material.color;
-            color.a = alpha;
-            renderer.material.color = color;
+        // Renderer renderer = transform.GetChild(0).GetComponent<Renderer>();
+        // if (renderer != null)
+        // {
+        //     Color color = renderer.material.color;
+        //     color.a = alpha;
+        //     renderer.material.color = color;
 
-            if (alpha < 1.0f)
-            {
-                renderer.material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-                renderer.material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-                renderer.material.SetInt("_ZWrite", 0);
-                renderer.material.DisableKeyword("_ALPHATEST_ON");
-                renderer.material.EnableKeyword("_ALPHABLEND_ON");
-                renderer.material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-                renderer.material.renderQueue = 3000;
-            }
-            else
-            {
-                renderer.material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-                renderer.material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
-                renderer.material.SetInt("_ZWrite", 1);
-                renderer.material.DisableKeyword("_ALPHABLEND_ON");
-                renderer.material.EnableKeyword("_ALPHATEST_ON");
-                renderer.material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-                renderer.material.renderQueue = -1;
-            }
-        }
+        //     if (alpha < 1.0f)
+        //     {
+        //         renderer.material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+        //         renderer.material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+        //         renderer.material.SetInt("_ZWrite", 0);
+        //         renderer.material.DisableKeyword("_ALPHATEST_ON");
+        //         renderer.material.EnableKeyword("_ALPHABLEND_ON");
+        //         renderer.material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+        //         renderer.material.renderQueue = 3000;
+        //     }
+        //     else
+        //     {
+        //         renderer.material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+        //         renderer.material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+        //         renderer.material.SetInt("_ZWrite", 1);
+        //         renderer.material.DisableKeyword("_ALPHABLEND_ON");
+        //         renderer.material.EnableKeyword("_ALPHATEST_ON");
+        //         renderer.material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+        //         renderer.material.renderQueue = -1;
+        //     }
+        // }
     }
 }
